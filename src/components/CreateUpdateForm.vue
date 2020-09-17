b23a3be03bd3dd76f2780387b3075cf61b3b379a
Arsen <arsen.utepbergenov@yandex.ru>
2020-09-17 17:05:29 +0300

    master/minor updates of details

--- src/components/CreateUpdateForm.vue
+++ src/components/CreateUpdateForm.vue
@@ -3,64 +3,66 @@
 <template>
   <div class="">
     <form class="form" @submit.prevent="updateContact">
-      <div class="">
-        <div class="form__content">
-          <div class="form__input-group">
-            <label class="form-label" for="contact-name">
-              Name
-            </label>
-            <input
-              id="contact-name"
-              type="text"
-              class="form__input"
-              v-model="updatedContact.name"
-              required />
-          </div>
-          <div class="form__input-group">
-            <label class="form-label" for="contact-phone">
-              Phone
-            </label>
-            <input
-              id="contact-phone"
-              type="text"
-              class="form__input"
-              v-model="updatedContact.phone"
-              required />
-          </div>
-          <div class="form__input-group">
-            <label class="form-label" for="contact-email">
-              Email
-            </label>
-            <input
-              id="contact-email"
-              type="email"
-              class="form__input"
-              v-model="updatedContact.email"
-              required />
-          </div>
-          <div class="form__input-group">
-            <progress class="form-progress" value="0" max="100" ref="progressImg">0%</progress>
-            <input style="display: none" type="file" @change="selectedImg" ref="inputImg" />
-          </div>
-          <div class="form__input-group">
-            <div class="">
-              File avatar:&nbsp;
-              <span>{{selectedImgName}}</span>
-            </div>
-          </div>
-          <div class="form__input-group">
-            <img v-if="imgUrl" :src="imgUrl" :alt="imgFileName" class="card__img">
-          </div>
-          <div class="form__input-group">
-            <div class="">
-              <button type="button" class="btn form__btn" @click="$refs.inputImg.click()">
-                Donwload
-              </button>
-              <button type="button" class="btn form__btn" @click="uploadUpdatedImg()" :disabled="isUpdloadDisabled">Updload</button>
-            </div>
-          </div>
-          <button type="submit" class="btn form__btn form--btn-create">{{textSubmitButton}}</button>
+      <div class="form__content">
+        <div class="form__input-group">
+          <label class="form-label" for="contact-name">
+            Name
+          </label>
+          <input
+            id="contact-name"
+            type="text"
+            class="form__input"
+            placeholder="Contact name..."
+            v-model="updatedContact.name"
+            required />
         </div>
+        <div class="form__input-group">
+          <label class="form-label" for="contact-phone">
+            Phone
+          </label>
+          <input
+            id="contact-phone"
+            type="tel"
+            pattern="\+79[0-9]{9}|8[0-9]{10}"
+            placeholder="+79..."
+            class="form__input"
+            v-model="updatedContact.phone"
+            required />
+        </div>
+        <div class="form__input-group">
+          <label class="form-label" for="contact-email">
+            Email
+          </label>
+          <input
+            id="contact-email"
+            type="email"
+            placeholder="test@test.com"
+            class="form__input"
+            v-model="updatedContact.email"
+            required />
+        </div>
+        <div class="form__input-group">
+          <progress class="form-progress" value="0" max="100" ref="progressImg">0%</progress>
+          <input style="display: none" type="file" @change="selectedImg" ref="inputImg" />
+        </div>
+        <div class="form__input-group">
+          <div class="">
+            File avatar:&nbsp;
+            <span>{{selectedImgName}}</span>
+          </div>
+        </div>
+        <div class="form__input-group">
+          <img v-if="imgUrl" :src="imgUrl" :alt="imgFileName" class="card__img">
+        </div>
+        <div class="form__input-group">
+          <div class="">
+            <button type="button" class="btn form__btn" @click="$refs.inputImg.click()">
+              Donwload
+            </button>
+            <button type="button" class="btn form__btn" @click="uploadUpdatedImg()" :disabled="isUpdloadDisabled">Updload</button>
+          </div>
+        </div>
+        <button type="submit" class="btn form__btn form--btn-create">{{textSubmitButton}}</button>
       </div>
     </form>
   </div>
--- src/views/ContactDetails.vue
+++ src/views/ContactDetails.vue
@@ -1,5 +1,43 @@
+// The contact details view
+
 <template>
-  <section class="contact-details">
-    <h1>This is an about page</h1>
+  <section v-if="isReady" class="contact-details">
+    <div class="container">
+      <div class="card">
+        <div class="contact-details__avatar">
+          <h2>{{contact.name}}</h2>
+          <img :src="contact.avatar" :alt="contact.name">
+        </div>
+        <div class="contact-details__info">
+          <p><b>Phone:</b> {{contact.phone}}</p>
+          <p><b>Email:</b> {{contact.email}}</p>
+          <p><b>About:</b> {{contact.about}}</p>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
+
+<script>
+import { fetchContact } from '../api/contact-book'
+
+export default {
+  name: 'contact-details',
+  data () {
+    return {
+      contactId: '',
+      contact: null,
+      isReady: false
+    }
+  },
+  async created () {
+    this.contactId = this.$route.params.id
+    this.contact = await fetchContact(this.contactId)
+    this.isReady = true
+  },
+  methods: {
+    updateBook () {
+    }
+  }
+}
+</script>
